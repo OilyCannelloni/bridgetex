3fc0521d49c096400afa24bb382852f5f089b0cd
oilycannelloni <bartek.slupik@gmail.com>
2025-07-15 17:05:10 +0200

    bridgetex

--- backend/controller.py
+++ backend/controller.py
@@ -9,14 +9,15 @@ import subprocess
 
 class LuaLatexManager:
     def __init__(self):
-        self.tmp_path = Path(tempfile.gettempdir())
+        self.tmp_path = Path(__file__).parent.resolve() / "temp"
         print(self.tmp_path)
 
     def compile(self, code: str):
         # Create file
         tex_path = self.tmp_path / "code.tex"
 
-        with open(tex_path, "w") as tf:
+        with open(tex_path, "w", encoding="utf-8") as tf:
+            tf.write("\\documentclass{article}\n\\usepackage{import}\n\\import{./lib/}{bridge.sty}\n")
             tf.write(code)
         
         # Compile
